21c24ea4aa86e09c87d2b12eed77546c5e01a558
Allan <allan.nunes.carelli@gmail.com>
2025-11-02 18:51:08 -0300

    mais bonito

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let IGV21 = document.getElementById("link_IGV21")
 let IGV10 = document.getElementById("link_IGV10")
+const linkss = document.getElementsByClassName("link")
 
-IGV21.addEventListener("mouseenter", function(){
-    IGV21.innerHTML = '<a href="https://deepwoken.co/builder?id=K1AJLVYm" id="link">Ignis Glacialis V2.1 (Sub Procella)</a>'
-})
-IGV21.addEventListener("mouseleave", function(){
-    IGV21.innerHTML = 'Ignis Glacialis V2.1 (Sub Procella)'
-})
 
-IGV10.addEventListener("mouseenter", function(){
-    IGV10.innerHTML = '<a href="https://deepwoken.co/builder?id=az0C9axo" id="link">Ignis Glacialis V1.0</a>'
+
+function builds_links(id, link_da_build, nome_da_build){
+id.addEventListener("mouseenter", function(){
+    id.innerHTML = `<a href="${link_da_build}" id="link">${nome_da_build}</a>`
 })
-IGV10.addEventListener("mouseleave", function(){
-    IGV10.innerHTML = 'Ignis Glacialis V1.0'
+id.addEventListener("mouseleave", function(){
+    id.innerHTML = nome_da_build
 })
+}
+
+builds_links(IGV21, "https://deepwoken.co/builder?id=K1AJLVYm", "Ignis Glacialis V2.1 (Sub Procella)")
+builds_links(IGV10, "https://deepwoken.co/builder?id=az0C9axo", "Ignis Glacialis V1.0")
+
+
