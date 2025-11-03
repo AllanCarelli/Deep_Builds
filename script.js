5894d6c6ec8e44f553b4e6e734e970631775aa1f
AllanCarelli <142117112+AllanCarelli@users.noreply.github.com>
2025-11-03 19:07:33 +0000

    silent soul

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let IGV21 = document.getElementById("link_IGV21")
 let IGV10 = document.getElementById("link_IGV10")
+let SLV10 = document.getElementById("link_SLV10")
 let discord = document.getElementById("discord")
 const linkss = document.getElementsByClassName("link")
 
@@ -16,6 +17,7 @@ id.addEventListener("mouseleave", function(){
 
 builds_links(IGV21, "https://deepwoken.co/builder?id=K1AJLVYm", "Ignis Glacialis V2.1 (Sub Procella)")
 builds_links(IGV10, "https://deepwoken.co/builder?id=az0C9axo", "Ignis Glacialis V1.0")
+builds_links(SLV10, "https://deepwoken.co/builder?id=7S4ervmz", "Silent Soul V1.0")
 
 
 
