0de417ea646d800cfa0a28bfcf432eb1a7420b8e
AllanCarelli <142117112+AllanCarelli@users.noreply.github.com>
2025-11-03 19:49:21 +0000

    js otimizado

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-let IGV21 = document.getElementById("link_IGV21")
-let IGV10 = document.getElementById("link_IGV10")
-let SLV10 = document.getElementById("link_SLV10")
-let discord = document.getElementById("discord")
-const linkss = document.getElementsByClassName("link")
-
-
-
 function builds_links(id, link_da_build, nome_da_build){
-id.addEventListener("mouseenter", function(){
-    id.innerHTML = `<a href="${link_da_build}" id="link">${nome_da_build}</a>`
+let id1 = document.getElementById(`link_${id}`)
+id1.addEventListener("mouseenter", function(){
+    id1.innerHTML = `<a href="${link_da_build}" id="link" target="_blank">${nome_da_build}</a>`
 })
-id.addEventListener("mouseleave", function(){
-    id.innerHTML = nome_da_build
+id1.addEventListener("mouseleave", function(){
+    id1.innerHTML = nome_da_build
 })
 }
 
-builds_links(IGV21, "https://deepwoken.co/builder?id=K1AJLVYm", "Ignis Glacialis V2.1 (Sub Procella)")
-builds_links(IGV10, "https://deepwoken.co/builder?id=az0C9axo", "Ignis Glacialis V1.0")
-builds_links(SLV10, "https://deepwoken.co/builder?id=7S4ervmz", "Silent Soul V1.0")
+builds_links("IGV21", "https://deepwoken.co/builder?id=K1AJLVYm", "Ignis Glacialis V2.1 (Sub Procella)")
+builds_links("IGV20", "https://deepwoken.co/builder?id=OT1dgk3r", "Ignis Glacialis V2.0")
+builds_links("IGV10", "https://deepwoken.co/builder?id=az0C9axo", "Ignis Glacialis V1.0")
+builds_links("SSV10", "https://deepwoken.co/builder?id=7S4ervmz", "Silent Soul V1.0")
 
 
 
